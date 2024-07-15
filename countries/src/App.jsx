150d2d35af6acaa19e12f4de8b59098161fae3e0
matthew Wong <matthew.wong128@gmail.com>
2024-07-15 14:01:55 -0400

    idk

--- countries/src/App.jsx
+++ countries/src/App.jsx
@@ -5,19 +5,33 @@ let listOfAllCountries = [];
 
 function App() {
   const [country, setCountry] = useState("");
-  const [countries, setCountries] = useState({});
+  const [countries, setCountries] = useState([]);
 
   useEffect(() => {
     Server.getAllCountries().then((list) => {
       listOfAllCountries = list;
-      console.log(listOfAllCountries);
+      // console.log(listOfAllCountries);
     });
   }, []);
 
-  const searchCountries = (query) => {};
+  const searchCountries = (query) => {
+    const queryResult = listOfAllCountries.filter((name) => name.includes(query));
+    if (queryResult.length > 10) return <div>{`Too many matches, results: ${queryResult.length}`}</div>;
+    if (queryResult.length == 0) return <div>{` No matches, results: ${queryResult.length} `}</div>;
+    return (
+      <div>
+        <ul>
+          {queryResult.map((country, i) => (
+            <li key={i}>{country}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  };
   const handleCountryChange = (event) => {
     event.preventDefault();
     setCountry(event.target.value);
+    console.log("------", searchCountries(event.target.value));
   };
 
   return (
