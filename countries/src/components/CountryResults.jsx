3dae93917a61e19cc720cd9091ffa1ef8276c7e5
matthew Wong <matthew.wong128@gmail.com>
2024-07-15 17:28:10 -0400

    made component for country detail

--- countries/src/components/CountryResults.jsx
+++ countries/src/components/CountryResults.jsx
@@ -1,26 +1,29 @@
 import React from "react";
 
+const CountryDetails = ({ country }) => {
+  const languages = Object.values(country.languages);
+  return (
+    <div>
+      <h1>
+        {country.flag} {country.name.common}
+      </h1>
+      <b>Languages</b>
+
+      <ul>
+        {languages.map((language, i) => (
+          <li key={i}>{language}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
 const CountryResults = ({ queryResult }) => {
   if (!queryResult) return null;
   if (queryResult.length > 10) return <div>{`Too many matches, results: ${queryResult.length}`}</div>;
   if (queryResult.length == 0) return <div>{` No matches, results: ${queryResult.length} `}</div>;
   if (queryResult.length == 1) {
-    const country = queryResult[0];
-    const languages = Object.values(country.languages);
-    return (
-      <div>
-        <h1>
-          {country.flag} {country.name.common}
-        </h1>
-        <b>Languages</b>
-
-        <ul>
-          {languages.map((language, i) => (
-            <li key={i}>{language}</li>
-          ))}
-        </ul>
-      </div>
-    );
+    return <CountryDetails country={queryResult[0]} />;
   }
 
   return (
