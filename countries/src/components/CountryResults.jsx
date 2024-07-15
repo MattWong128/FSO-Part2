95bbe79130af54b40944a135d264530293af5053
matthew Wong <matthew.wong128@gmail.com>
2024-07-15 18:14:44 -0400

    show&hide details works

--- countries/src/components/CountryResults.jsx
+++ countries/src/components/CountryResults.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const CountryDetails = ({ country }) => {
   const languages = Object.values(country.languages);
@@ -19,6 +19,8 @@ const CountryDetails = ({ country }) => {
 };
 
 const CountryResults = ({ queryResult }) => {
+  const [selectedCountries, setSelectedCountries] = useState([]);
+  console.log(selectedCountries);
   if (!queryResult) return null;
   if (queryResult.length > 10) return <div>{`Too many matches, results: ${queryResult.length}`}</div>;
   if (queryResult.length == 0) return <div>{` No matches, results: ${queryResult.length} `}</div>;
@@ -26,11 +28,24 @@ const CountryResults = ({ queryResult }) => {
     return <CountryDetails country={queryResult[0]} />;
   }
 
+  const handleShow = (country) => {
+    if (!isCountrySelected(country)) {
+      setSelectedCountries(selectedCountries.concat(country));
+    } else {
+      setSelectedCountries(selectedCountries.filter((c) => c.name.common != country.name.common));
+    }
+  };
+  const isCountrySelected = (country) => selectedCountries.includes(country);
   return (
     <>
       {queryResult.map((country) => {
         const name = country.name.common;
-        return <li key={name}>{name}</li>;
+        return (
+          <li key={name}>
+            {name} <button onClick={() => handleShow(country)}>{!isCountrySelected(country) ? "show" : "hide"}</button>
+            {selectedCountries.includes(country) && <CountryDetails country={country} />}
+          </li>
+        );
       })}
     </>
   );
