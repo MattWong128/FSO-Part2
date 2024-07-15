9ebced715900f1ca50bbb612cee2ac50ff995173
matthew Wong <matthew.wong128@gmail.com>
2024-07-15 15:34:49 -0400

    search done

--- countries/src/components/CountryResults.jsx
+++ countries/src/components/CountryResults.jsx
@@ -5,13 +5,11 @@ const CountryResults = ({ queryResult }) => {
   if (queryResult.length > 10) return <div>{`Too many matches, results: ${queryResult.length}`}</div>;
   if (queryResult.length == 0) return <div>{` No matches, results: ${queryResult.length} `}</div>;
   return (
-    <div>
-      <ul>
-        {queryResult.map((country) => (
-          <li key={country}>{country}</li>
-        ))}
-      </ul>
-    </div>
+    <>
+      {queryResult.map((country) => (
+        <li key={country}>{country}</li>
+      ))}
+    </>
   );
 };
 
