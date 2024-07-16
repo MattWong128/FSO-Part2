2acd12b1a16aec8092e318cd2dd3c92d0adee1d4
matthew Wong <matthew.wong128@gmail.com>
2024-07-16 14:25:15 -0400

    weather aoi working

--- countries/src/components/Weather.jsx
+++ countries/src/components/Weather.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import axios from "axios";
+const API_KEY = "02d5025c3384c3caa87f8b3c95b43491";
 
 const getCountryCoordinates = (country) => {
   const capital = country.capital[0];
-  const API_KEY = "02d5025c3384c3caa87f8b3c95b43491";
   const geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${capital}&appid=${API_KEY}`;
   return axios.get(geoURL).then((res) => {
     const lat = res.data[0].lat;
@@ -11,12 +11,18 @@ const getCountryCoordinates = (country) => {
     return { lat, lon };
   });
 };
+const getWeather = (country) => {
+  getCountryCoordinates(country).then((coords) => {
+    const lat = coords.lat;
+    const lon = coords.lon;
+
+    const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`;
+    axios.get(weatherUrl).then((res) => {
+      console.log(res.data.main.temp);
+    });
+  });
+};
 const Weather = ({ country }) => {
-  console.log(getCountryCoordinates(country));
-  //   return getCountryCoordinates(country).then((coordinates) => {
-  //     const lat = coordinates.lat;
-  //     const lon = coordinates.lon;
-  //     // return <div>{/* {lat} {lon} */}</div>;
-  //   });
+  getWeather(country);
 };
 export default Weather;
