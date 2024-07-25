53ac58907c39dcbc01645bd9ebd1c5d8d71600d8
matthew Wong <matthew.wong128@gmail.com>
2024-07-25 13:33:20 -0400

    added optimistic ui for adding

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -1,18 +1,18 @@
-import { useState, useEffect } from "react";
-import Filter from "./components/Filter";
-import PersonForm from "./components/PersonForm";
-import Persons from "./components/Persons";
-import Server from "./services/Server";
-import Notification from "./components/Notification";
-import "./index.css";
+import { useState, useEffect } from 'react';
+import Filter from './components/Filter';
+import PersonForm from './components/PersonForm';
+import Persons from './components/Persons';
+import Server from './services/Server';
+import Notification from './components/Notification';
+import './index.css';
 
 const App = () => {
   const [persons, setPersons] = useState([]);
-  const [newName, setNewName] = useState("");
-  const [newNumber, setNewNumber] = useState("");
-  const [search, setSearch] = useState("");
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const [search, setSearch] = useState('');
   const [message, setMessage] = useState(null);
-  console.log("Current persons:", persons);
+  console.log('Current persons:', persons);
 
   const queryResult = persons.filter((person) => person.name.includes(search));
 
@@ -37,12 +37,12 @@ const App = () => {
     if (doesNameExist) {
       if (window.confirm(`${newName} is already added to the phone book, replace the old number with a new one?`)) {
         const personTochange = persons.find((person) => person.name == newName);
-        console.log("-------".personTochange);
         const updatedPerson = { ...personTochange, number: newNumber };
+        setPersons(persons.map((person) => (person.id !== updatedPerson.id ? person : updatedPerson)));
 
         Server.update(updatedPerson)
           .then((returnedPerson) => {
-            setPersons(persons.map((person) => (person.id !== updatedPerson.id ? person : returnedPerson)));
+            // setPersons(persons.map((person) => (person.id !== updatedPerson.id ? person : returnedPerson)));
           })
           .catch((err) => {
             setMessage(`Information of ${updatedPerson.name} has already been removed`);
@@ -65,12 +65,12 @@ const App = () => {
       setTimeout(() => {
         setMessage(null);
       }, 5000);
-      setNewName("");
-      setNewNumber("");
+      setNewName('');
+      setNewNumber('');
     });
 
     console.log(persons);
-    console.log("adding new persone: ", newPersonObj);
+    console.log('adding new persone: ', newPersonObj);
   };
   const deletePerson = (id) => {
     const name = persons.find((p) => p.id == id).name;
